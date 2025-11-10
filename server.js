c118ef5ce45d66cf2d1319df546b17ea663dd961
lkyap70-netizen <lkyap70@gmail.com>
2025-11-10 09:35:26 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send('File uploaded');
+});
+
+app.listen(3000);
